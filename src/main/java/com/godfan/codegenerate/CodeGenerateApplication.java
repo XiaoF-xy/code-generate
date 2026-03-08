2936366fccb6b1a77dfbd25266168b232df70571
fxy <2728943961@qq.com>
2026-03-08 13:59:48 +0800

    feat(user): 用户模块后端 1.开发 Mybatis Flex 代码生成器 2.开发用户模块后端接口 3.管理员增删改查 4.全局校验aop 5.自定义配置解决丢失精度问题

--- src/main/java/com/godfan/codegenerate/CodeGenerateApplication.java
+++ src/main/java/com/godfan/codegenerate/CodeGenerateApplication.java
@@ -1,9 +1,11 @@
 package com.godfan.codegenerate;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.godfan.codegenerate.mapper")
 public class CodeGenerateApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/godfan/codegenerate/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.godfan.codegenerate.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.godfan.codegenerate.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author GodFan
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
