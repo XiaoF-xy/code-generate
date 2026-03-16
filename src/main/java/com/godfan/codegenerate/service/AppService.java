4ee4a593e0b9d9fda45c3ca8a6427711b6c614d5
fxy <2728943961@qq.com>
2026-03-16 15:58:41 +0800

    应用模块（完成基本功能） 1.AI生产基础代码 2。完成基本功能人工测试通过

--- src/main/java/com/godfan/codegenerate/service/AppService.java
+++ src/main/java/com/godfan/codegenerate/service/AppService.java
@@ -1,7 +1,12 @@
 package com.godfan.codegenerate.service;
 
+import com.godfan.codegenerate.model.dto.app.AppQueryRequest;
+import com.godfan.codegenerate.model.vo.AppVO;
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
 import com.godfan.codegenerate.model.entity.App;
+
+import java.util.List;
 
 /**
  * 应用 服务层。
@@ -9,5 +14,24 @@ import com.godfan.codegenerate.model.entity.App;
  * @author GodFan
  */
 public interface AppService extends IService<App> {
+    /**
+     * 获取应用封装类
+     * @param app
+     * @return
+     */
+    AppVO getAppVO(App app);
 
+    /**
+     * 获取应用封装类列表
+     * @param appList
+     * @return
+     */
+    List<AppVO> getAppVOList(List<App> appList);
+
+    /**
+     * 构造应用查询条件
+     * @param appQueryRequest
+     * @return
+     */
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
 }
