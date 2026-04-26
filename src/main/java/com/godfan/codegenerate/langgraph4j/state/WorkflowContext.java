6b65ad4fc74c461e8bd104098413bd7aa2e0eca8
fxy <2728943961@qq.com>
2026-04-26 22:37:27 +0800

    1.条件边 - 可选进入项目构造节点 2.循环边 - 代码质量检查

--- src/main/java/com/godfan/codegenerate/langgraph4j/state/WorkflowContext.java
+++ src/main/java/com/godfan/codegenerate/langgraph4j/state/WorkflowContext.java
@@ -1,6 +1,7 @@
 package com.godfan.codegenerate.langgraph4j.state;
 
 import com.godfan.codegenerate.langgraph4j.model.ImageResource;
+import com.godfan.codegenerate.langgraph4j.model.QualityResult;
 import com.godfan.codegenerate.model.enums.CodeGenTypeEnum;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -66,6 +67,11 @@ public class WorkflowContext implements Serializable {
      * 构建成功的目录
      */
     private String buildResultDir;
+
+    /**
+     * 质量检查结果
+     */
+    private QualityResult qualityResult;
 
     /**
      * 错误信息
