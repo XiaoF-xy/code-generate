7fb229ff146b848f120b7bea0028a6edb9f477a7
fxy <2728943961@qq.com>
2026-05-24 15:59:19 +0800

    Prometheus + Grafana 业务监控

--- src/main/java/com/godfan/codegenerate/service/impl/AppServiceImpl.java
+++ src/main/java/com/godfan/codegenerate/service/impl/AppServiceImpl.java
@@ -21,6 +21,8 @@ import com.godfan.codegenerate.model.enums.ChatHistoryMessageTypeEnum;
 import com.godfan.codegenerate.model.enums.CodeGenTypeEnum;
 import com.godfan.codegenerate.model.vo.AppVO;
 import com.godfan.codegenerate.model.vo.UserVO;
+import com.godfan.codegenerate.monitor.MonitorContext;
+import com.godfan.codegenerate.monitor.MonitorContextHolder;
 import com.godfan.codegenerate.service.ChatHistoryService;
 import com.godfan.codegenerate.service.ScreenshotService;
 import com.godfan.codegenerate.service.UserService;
@@ -155,10 +157,21 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App>  implements AppS
         }
         //5.在调用AI先保存用户消息到数据库中
         chatHistoryService.addChatMessage(appId, message, ChatHistoryMessageTypeEnum.USER.getValue(), loginUser.getId());
-        // 6. 调用 AI 生成代码（流式）
+        // 6. 设置监控上下文
+        MonitorContextHolder.setContext(
+                MonitorContext.builder()
+                        .userId(loginUser.getId().toString())
+                        .appId(appId.toString())
+                        .build()
+        );
+        // 7. 调用 AI 生成代码（流式）
         Flux<String> codeStream = aiCodeGeneratorFacade.generateAndSaveCodeStream(message, codeGenTypeEnum, appId);
-        // 7. 收集AI响应内容并在完成后记录到对话历史
-        return  streamHandlerExecutor.doExecute(codeStream, chatHistoryService, appId, loginUser, codeGenTypeEnum);
+        // 8. 收集AI响应内容并在完成后记录到对话历史
+        return streamHandlerExecutor.doExecute(codeStream, chatHistoryService, appId, loginUser, codeGenTypeEnum)
+                .doFinally(signalType -> {
+                    // 流结束时清理（无论成功/失败/取消）
+                    MonitorContextHolder.clearContext();
+                });
     }
 
     @Override
